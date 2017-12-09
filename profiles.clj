51739889f69aed4a2ccaf751d13dca502dd92c1f
markaddleman <markaddleman@gmail.com>
2017-12-09 07:54:36 -0800

    init

---
+++ profiles.clj
@@ -0,0 +1,16 @@
+{:dev {:plugins      [[lein-ancient "0.6.14"]]
+       :dependencies [[com.cognitect/transcriptor "0.1.5"]
+                      [org.clojure/tools.namespace "0.2.11"]
+                      [vvvvalvalval/scope-capture "0.1.4"]
+                      [inspectable "0.2.2"]
+                      [spec-provider "0.4.10"]
+                      [alembic "0.3.2"]]
+       :injections   [(require '[sc.api :as -sc])
+                      (require '[clojure.tools.namespace.repl :refer [refresh]])
+                      (require '[cognitect.transcriptor :as -xr :refer (check!)])
+                      (require '[inspectable.repl :refer [why browse-spec]])
+                      (require '[clojure.spec.test.alpha :as -st])
+                      (require '[alembic.still :refer [load-project]])
+                      (refresh)
+                      (-st/instrument)]
+       :source-paths ["dev"]}}
