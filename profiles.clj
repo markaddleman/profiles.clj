de0e7d69af00438ebd61a6e5f692a959eab5f55d
Mark Addleman <markaddleman@gmail.com>
2018-02-17 08:24:32 -0800

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -4,13 +4,21 @@
                       [vvvvalvalval/scope-capture "0.1.4"]
                       [inspectable "0.2.2"]
                       [spec-provider "0.4.10"]
-                      [alembic "0.3.2"]]
-       :injections   [(require '[sc.api :as -sc])
+                      [alembic "0.3.2"]
+                      [venantius/pyro "0.1.1"]
+                      [jsofra/data-scope "0.1.2"]]
+       :injections   [(require '[pyro.printer :as printer])
+                      (printer/swap-stacktrace-engine!) 
+                      (require '[sc.api :as -sc])
                       (require '[clojure.tools.namespace.repl :refer [refresh]])
                       (require '[cognitect.transcriptor :as -xr :refer (check!)])
                       (require '[inspectable.repl :refer [why browse-spec]])
                       (require '[clojure.spec.test.alpha :as -st])
                       (require '[alembic.still :refer [load-project]])
+                      (require 'data-scope.charts)
+                      (require 'data-scope.graphs)
+                      (require 'data-scope.inspect)
+                      (require 'data-scope.pprint)
                       (refresh)
                       (-st/instrument)]
        :source-paths ["dev"]}}
